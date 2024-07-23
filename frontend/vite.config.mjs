f4ea0094f5e1fe1287838f291db3fa291d7ffb18
Victoria Raya <victoriaraya@Victorias-Air.attlocal.net>
2024-07-22 23:06:34 -0700

    change vite config for images

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,4 +4,17 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").pop();
+          if (/png|jpe?g|gif|svg|webp|avif/i.test(extType)) {
+            return "assets/[name]-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
