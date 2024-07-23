7a0b94648d47936c32f595b1d911183952d5ff80
Victoria Raya <victoriaraya@Victorias-Air.attlocal.net>
2024-07-23 15:07:30 -0700

    fix production image test

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,17 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").pop();
-          if (/png|jpe?g|gif|svg|webp|avif/i.test(extType)) {
-            return "assets/[name]-[hash][extname]";
-          }
-          return "assets/[name]-[hash][extname]";
-        },
-      },
-    },
-  },
 });
